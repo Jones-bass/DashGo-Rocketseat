518ea6faefa3d44d4a866fcdc3b0efa431c30ab5
Jones-bass <jonesbass.tb@gmail.com>
2022-04-17 21:28:59 -0300

    config colors theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
+import { AppProps } from "next/app"
+import { theme } from '../styles/theme'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
