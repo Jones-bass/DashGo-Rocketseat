460f694908a1a500131ba502f8d9108de57fbba4
Jones-bass <jonesbass.tb@gmail.com>
2022-04-20 14:54:12 -0300

    open close navebar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean; 
+}
+export function Profile({ showProfileData = true}: ProfileProps) {
   return (
     <Flex align='center'>
+      { showProfileData && (
         <Box mr='6' textAlign='right'>
           <Text>Jones Bass</Text>
           <Text color='gray.300' fontSize='small'>
             jonesbass.tb@gmail.com
           </Text>
         </Box>
+        )}
       <Avatar size='md' name='Jones Bass' src='https://avatars.githubusercontent.com/u/85463497?v=4' />
     </Flex>
   )
